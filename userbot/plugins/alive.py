34f35ebf4b92701c8feeae70dacdc71d9f22f48b
shubhdevil <62930655+shubhdevil@users.noreply.github.com>
2020-04-27 16:41:10 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,4 +1,3 @@
-"""Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
 import asyncio
 from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
@@ -6,7 +5,7 @@ from platform import uname
 from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check pinned in @XtraTgBot"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba"
 
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
